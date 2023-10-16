5edcdf24afcdcb831e10eea01934a68ab4bad463
somecreater <dbstjrgy123@gmail.com>
2023-10-16 16:28:24 +0900

    2023년 10월 16일자 스프링학습(474page)

--- ex04/src/test/java/org/zerock/service/SampleTxServiceTests.java
+++ ex04/src/test/java/org/zerock/service/SampleTxServiceTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -12,25 +13,22 @@ import lombok.extern.log4j.Log4j;
 @RunWith(SpringJUnit4ClassRunner.class)
 @Log4j
 @ContextConfiguration({"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
-public class SampleServiceTests {
+public class SampleTxServiceTests {
 	
-	@Setter(onMethod_=@Autowired)
-	private SampleService service;
+	@Setter(onMethod_= {@Autowired})
+	private SampleTxService service;
 	
-	//@Test
-	public void testClass() {
-		
-		log.info(service);
-		log.info(service.getClass().getName());
-	}
-	
-	@Test
-	public void testAdd() throws Exception{
-		log.info(service.doAdd("123", "456"));
-	}
-	
-	//@Test
-	public void testAddError() throws Exception{
-		log.info(service.doAdd("123", "ABC"));
-	}
+	  @Test
+	  public void testLong() {
+	    
+	    String str ="Starry\r\n" + 
+	        "Starry night\r\n" + 
+	        "Paint your palette blue and grey\r\n" + 
+	        "Look out on a summer's day";
+	    
+	    log.info(str.getBytes().length);
+	    
+	    service.addData(str);    
+	  }
+	  
 }
